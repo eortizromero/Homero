c2a2ed044e0e4f35292c549f586e72c1087e9882
Edgar Ortiz <edgardoficial.yo@gmail.com>
2018-01-19 17:58:02 -0600

    Nuevas funciones agregadas a la App

--- homero/app.py
+++ homero/app.py
@@ -8,48 +8,67 @@ package = 'uncompyle6'
 try:
     __import__('%s' % package)
 except:
-    # TODO: valid current os for custom command
+    # Validar sistema operativo actual para instalar la librería, usando el comando correcto.
     from sys import platform
 
-    command = os.system('gksudo pip install %s' % package) \
+    comando = os.system('gksudo pip install %s' % package) \
         if platform == 'linux' or platform == 'linux2' else os.system('pip install %s' % package)
 
 
-def find(patterns, path):
-    result = []
-    for root, dirs, files in os.walk(path):
-        for name in files:
-            if isinstance(patterns, list):
-                for pattern in patterns:
-                    if fnmatch.fnmatch(name, pattern):
-                        result.append(os.path.join(root, name))
-            else:
-                raise TypeError("You need a list as the first argument, instead of %s" % type(patterns))
-    return result
+class App(object):
+    def __init__(self):
+        pass
+
+    def buscar_archivos(self, patrones, ruta):
+        """
+            Método que ``busca`` los archivos `.pyc` en la ruta que se le asigna.
+            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        :param patrones: Una lista con el patrón o patrones a descompilar
+        :param ruta: Una cadena con la ruta en donde se buscarán los archivos.
+        :return: Una lista con la ruta o las rutas encontradas con archivos.
+        """
+        resultado = []
+        for raiz, dirs, archivos in os.walk(ruta):
+            for name in archivos:
+                if isinstance(patrones, list):
+                    for patron in patrones:
+                        if fnmatch.fnmatch(name, patron):
+                            resultado.append(os.path.join(raiz, name))
+                else:
+                    raise TypeError("Debes agregar una lista como primer parámetro, en lugar de %s" % type(patrones))
+        return resultado
+
+    def descompilar(self, rutalocal=None, ruta_busqueda=None, descompilar=False, descompilar_init=False):
+        """
+            Método que ``descompila`` los archivos `.pyc a .py`
+            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        :param rutalocal: Una cadena con la ruta en donde se guardarán los archivos descompilados.
+        :param ruta_busqueda: Una cadena con la ruta de busqueda de los archivos.
+        :param descompilar: Un boleano para los tests, no descompila en `False`
+        :param descompilar_init: Un boleano para descompilar los archivos `__init__.pyc`
+        :return: Los archivos descompilados o un error si hubo un problema.
+        """
+        # file_init = None
+        # TODO: restructurar esta sección para evitar sobreescribir archivos
+        comando = None
+        uncompile_all = True
+        init_file = None
+        if descompilar_init:
+            init_file = '*__init__.pyc'
+        archivos = self.buscar_archivos(['*.pyc'], ruta_busqueda if ruta_busqueda is not None else '/home')
+        for archivo in archivos:
+            if descompilar_init:
+                if '__init__.pyc' in archivo:
+                    pass
+            print("# All pyc archivos", archivo)
+            comando = '%s' % archivo
+        if descompilar:
+            # TODO: Verificar si el commando funciona con windows.
+            os.system('uncompyle6 -o %s %s' % (rutalocal, comando))
 
 
-def descompilar(local_path=None, from_uncompile_path=None, desc=False, uncompile_init=False):
-    # file_init = None
-    # TODO: restructurar esta sección para evitar sobreescribir archivos
-    command = None
-    uncompile_all = True
-    init_file = None
-    if uncompile_init:
-        init_file = '*__init__.pyc'
-        # init = find(['*__init__.pyc'], from_uncompile_path)
-        # for f in init:
-        #     print("# Init files ", f)
-        #     command = '%s' % f
-    files = find(['*.pyc'], from_uncompile_path if from_uncompile_path is not None else '/home')
-    for file in files:
-        if uncompile_init:
-            if '__init__.pyc' in file:
-                pass
-        print("# All pyc files", file)
-        command = '%s' % file
-    if desc:
-        os.system('uncompyle6 -o %s %s' % (local_path, command))
+app = App()
 
 
 if __name__ == '__main__':
-    descompilar('/home/eortiz/prueba/', '/home/eortiz/odoo_addons', desc=False, uncompile_init=True)
+    app.descompilar('/home/eortiz/prueba/', '/home/eortiz/odoo_addons', descompilar=False, descompilar_init=True)
